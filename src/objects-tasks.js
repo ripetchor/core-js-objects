cccbb0f11546d14ef470434e3bf09848415ea241
ripetchor <zhaxongir.sirozhiddinov@gmail.com>
2025-02-15 01:47:33 +0500

    feat: add mergeObjects solution

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -34,8 +34,13 @@ function shallowCopy(obj) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return objects
+    .flatMap((obj) => Object.entries(obj))
+    .reduce((acc, [key, value]) => {
+      acc[key] = (acc[key] || 0) + value;
+      return acc;
+    }, {});
 }
 
 /**
